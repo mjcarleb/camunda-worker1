279051010d5229aee436b1c119dec81f37f9106a
Mark Carlebach <markcarlebach@macbook-pro.myfiosgateway.com>
2022-05-03 07:43:29 -0400

    moved service task to complete with zbc.NewClient() parameter gatewayAddr := df4c514a-5a6b-45a8-8bf3-1c147540a2f4.bru-2.zeebe.camunda.io:443

--- main.go
+++ main.go
@@ -3,9 +3,14 @@ package main
 import (
 	"context"
 	"fmt"
+	"github.com/camunda/zeebe/clients/go/v8/pkg/entities"
 	"github.com/camunda/zeebe/clients/go/v8/pkg/pb"
+	"github.com/camunda/zeebe/clients/go/v8/pkg/worker"
 	"github.com/camunda/zeebe/clients/go/v8/pkg/zbc"
+	"log"
 )
+
+var readyClose = make(chan struct{})
 
 func main() {
 
@@ -41,6 +46,25 @@ func main() {
 			fmt.Println("  Partition", partition.PartitionId, ":", roleToString(partition.Role))
 		}
 	}
+
+	// Add code to do some work
+	gatewayAddr := "df4c514a-5a6b-45a8-8bf3-1c147540a2f4.bru-2.zeebe.camunda.io:443"
+	plainText := false
+
+	zbClient, err := zbc.NewClient(&zbc.ClientConfig{
+		GatewayAddress:         gatewayAddr,
+		UsePlaintextConnection: plainText,
+		CredentialsProvider:    credsProvider,
+	})
+
+	if err != nil {
+		panic(err)
+	}
+	jobWorker := zbClient.NewJobWorker().JobType("ext_sample_worker").Handler(handleJob).Open()
+
+	<-readyClose
+	jobWorker.Close()
+	jobWorker.AwaitClose()
 }
 
 func roleToString(role pb.Partition_PartitionBrokerRole) string {
@@ -51,5 +75,54 @@ func roleToString(role pb.Partition_PartitionBrokerRole) string {
 		return "Follower"
 	default:
 		return "Unknown"
+	}
+}
+
+func handleJob(client worker.JobClient, job entities.Job) {
+	jobKey := job.GetKey()
+
+	headers, err := job.GetCustomHeadersAsMap()
+	if err != nil {
+		// failed to handle job as we require the custom job headers
+		failJob(client, job)
+		return
+	}
+
+	variables, err := job.GetVariablesAsMap()
+	if err != nil {
+		// failed to handle job as we require the variables
+		failJob(client, job)
+		return
+	}
+
+	variables["totalPrice"] = 46.50
+	request, err := client.NewCompleteJobCommand().JobKey(jobKey).VariablesFromMap(variables)
+	if err != nil {
+		// failed to set the updated variables
+		failJob(client, job)
+		return
+	}
+
+	log.Println("Complete job", jobKey, "of type", job.Type)
+	log.Println("Processing order:", variables["orderId"])
+	log.Println("Collect money using payment method:", headers["method"])
+
+	ctx := context.Background()
+	_, err = request.Send(ctx)
+	if err != nil {
+		panic(err)
+	}
+
+	log.Println("Successfully completed job")
+	close(readyClose)
+}
+
+func failJob(client worker.JobClient, job entities.Job) {
+	log.Println("Failed to complete job", job.GetKey())
+
+	ctx := context.Background()
+	_, err := client.NewFailJobCommand().JobKey(job.GetKey()).Retries(job.Retries - 1).Send(ctx)
+	if err != nil {
+		panic(err)
 	}
 }
